8aef6bc49f71de60957cdb71a5fd9c4d9d0cd52d
Jonathan Tovar Diaz <jonitiso@gmail.com>
2021-09-27 19:34:17 -0500

    fixs

---
+++ custom-addons/example/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Contabilidad by SHF',
+    'description': 'Extensión de contabilidad por SHF',
+    'author': 'SHF SAS',
+    'depends': ['base'],
+    'application': False,
+    'version': '13.0.0',
+    'summary': 'Ejemplo vacio',
+    'installable': False,
+    'data': []
+}
